56cc6a7cbf283289bca57d9b93e5c09438b7a1a5
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-09 22:16:46 -0500

    home-routes.js and blogRoutes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Blog, User, Comment } = require("../models"); // change project and user
 const withAuth = require("../utils/auth");
 
+// --------------------------- GET All Router -------------------------------- //
 router.get("/", async (req, res) => {
   try {
     const blogData = await Blog.findAll({
@@ -23,6 +24,58 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+// --------------------------- GET one by ID Router -------------------------------- //
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render("blog", {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// --------------------------- Use withAuth middleware to prevent access to route -------------------------------- //
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// --------------------------- Login -------------------------------- //
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
